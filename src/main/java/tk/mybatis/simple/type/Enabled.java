ec28a78056d120b1618835547653981ad78c7b0b
seasky-wjl <2981210448@qq.com>
2023-02-14 14:24:25 +0800

    tested chapter 6

--- src/main/java/tk/mybatis/simple/type/Enabled.java
+++ src/main/java/tk/mybatis/simple/type/Enabled.java
@@ -5,6 +5,16 @@ package tk.mybatis.simple.type;
  * @create 2023-02-13-16:48
  */
 public enum Enabled {
-    disabled, //禁用
-    enabled;  //启用
+    disabled(0), //禁用
+    enabled(1);  //启用
+
+    private final int value;
+
+    Enabled(int value) {
+        this.value = value;
+    }
+
+    public int getValue(){
+        return value;
+    }
 }
